8ad21d417c0ccbd2df23885a3f15070d3910aafb
John Murwin <john.murwin@gmail.com>
2021-03-13 13:05:40 -0500

    Base Conjured Item Degradation Code for Check
    - Added Conjured Item quality decrease check in GildedRose.Java

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -18,35 +18,45 @@ class GildedRose
     }
 
     /*
-        *FUNCTION NAME
-        - Use Case:
+        *updateQuality
+        - Use Case: programtically updates the SellIn date and Quality of each items
         !input:
 
         ?output:
     */
     public void updateQuality() 
     {
-        for (int i = 0; i < items.length; i++) {
+        for (int i = 0; i < items.length; i++) 
+        {
             if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) 
+            {
+                if (items[i].quality > 0) 
+                {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) 
+                {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < 50) 
+                {
                     items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) 
+                    {
+                        if (items[i].sellIn < 11) 
+                        {
+                            if (items[i].quality < 50) 
+                            {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6) 
+                        {
+                            if (items[i].quality < 50) 
+                            {
                                 items[i].quality = items[i].quality + 1;
                             }
                         }
@@ -54,15 +64,30 @@ class GildedRose
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            //* Conjured Item Degradation
+            if (items[i].name.equals("Conjured Mana Cake"))
+            {
+                if (items[i].sellIn > 0)
+                    items[i].quality = items[i].quality - 1;
+                else
+                    items[i].quality = items[i].quality - 2;
+
+            }
+
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros"))
+            {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (items[i].sellIn < 0) 
+            {
+                if (!items[i].name.equals("Aged Brie")) 
+                {
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) 
+                    {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) 
+                            {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
@@ -70,7 +95,8 @@ class GildedRose
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < 50) 
+                    {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
